8e31b5c3692af2274ddcbb2c62298cad29a7b99d
DavidGVelez <davidgavilanesvelez@gmail.com>
2021-11-14 11:09:23 +0100

    Heroku testing

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    dd('testing');
+});
+
 Route::group(['prefix' => '/bnet'], function () {
 
     Route::get('/token/refresh', 'BnetController@refresh_access_token');
