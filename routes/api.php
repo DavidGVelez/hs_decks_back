fe31abcef1b9b8baf7b8fe437808427f859ca9ee
DavidGVelez <davidgavilanesvelez@gmail.com>
2022-05-22 09:48:01 +0200

    added blank route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('', function () {
+    return response()->json('Ok', 200);
+});
+
 Route::get('/ping', function () {
     return response()->json('Pong', 200);
 });
